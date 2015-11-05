1dd9ba5377eb2e2862331dd02d3196d2793775c6
Jack Medley <jack.medley@ed.ac.uk>
2015-11-05 17:04:01 +0000

    Prettied up a bit - pre HEJ drupal

--- Examples/gg2uux.py
+++ Examples/gg2uux.py
@@ -1,44 +1,44 @@
 #! /usr/bin/env python
 
 # Import spinor library
-import PySpinor as pys
-from PySpinor.Common import *
+from PySpinor import *
+
 
 #
 # Define momenta and spinors
 #
 
-pa = pys.Momenta(7.500000e+02, 0.000000e+00, 0.000000e+00, 7.500000e+02, incoming=True)  # Incoming gluon
-pb = pys.Momenta(7.500000e+02, 0.000000e+00, 0.000000e+00,-7.500000e+02, incoming=True)  # Incoming gluon
-p1 = pys.Momenta(7.500000e+02, 1.663864e+02, 6.672462e+02,-2.993294e+02)		 # Outgoing ux
-p2 = pys.Momenta(7.500000e+02,-1.663864e+02,-6.672462e+02, 2.993294e+02)		 # Outgoing u
+pa = Momenta(7.500000e+02, 0.000000e+00, 0.000000e+00, 7.500000e+02, incoming=True)  # Incoming gluon
+pb = Momenta(7.500000e+02, 0.000000e+00, 0.000000e+00,-7.500000e+02, incoming=True)  # Incoming gluon
+p1 = Momenta(7.500000e+02, 1.663864e+02, 6.672462e+02,-2.993294e+02)		 # Outgoing ux
+p2 = Momenta(7.500000e+02,-1.663864e+02,-6.672462e+02, 2.993294e+02)		 # Outgoing u
 
 # Check the phase space point conserves momentum
-assert pys.Momenta.Conserved() is True, "ERROR! Momentum not conserved..."
+assert Momenta.Conserved() is True, "ERROR! Momentum not conserved..."
 
 # Define spinors
-u_a = pys.Spinor(pa)
-u_b = pys.Spinor(pb)
-v_1 = pys.Spinor(p1, fermion=False)
-u_2 = pys.Spinor(p2)
+u_a = Spinor(pa)
+u_b = Spinor(pb)
+v_1 = Spinor(p1, fermion=False)
+u_2 = Spinor(p2)
 
 # Select only one polarisation
 pols = [('+', '+', '+', '-')]
 # pols = pols[1]
 
 # Define the gluon currents
-Gluon_a = pys.Gluon(pa, p1, 'nu')['+']
-Gluon_b = pys.Gluon(pb, p2, 'mu')['-']
+Gluon_a = Gluon(pa, p1, 'nu')['+']
+Gluon_b = Gluon(pb, p2, 'mu')['-']
 
 #
 # s-channel
 #
 
-term_1 = pys.Current(u_2.bar(), 'mu', v_1).dot(pb - pa) * Gluon_b.dot(Gluon_a)
-term_2 = pys.Current(u_2.bar(), 'mu', v_1).dot(Gluon_a) * Gluon_b.dot(pa)
-term_3 = pys.Current(u_2.bar(), 'mu', v_1).dot(Gluon_b) * Gluon_a.dot(pb)
+term_1 = Current(u_2.bar(), 'mu', v_1).dot(pb - pa) * Gluon_b.dot(Gluon_a)
+term_2 = Current(u_2.bar(), 'mu', v_1).dot(Gluon_a) * Gluon_b.dot(pa)
+term_3 = Current(u_2.bar(), 'mu', v_1).dot(Gluon_b) * Gluon_a.dot(pb)
 
-A_s = (term_1 + 2.0 * term_2 - 2.0 * term_3) / pys.s(pa, pb)
+A_s = (term_1 + 2.0 * term_2 - 2.0 * term_3) / s(pa, pb)
 
 print "   -> A_s = ", A_s[pols]
 
@@ -46,10 +46,10 @@ print "   -> A_s = ", A_s[pols]
 # t-channel
 #
 
-term_4  = pys.Current(u_2.bar(), 'mu', u_a('+')).dot(Gluon_b) * pys.Current(u_a.bar('+'), 'mu', v_1).dot(Gluon_a)
-term_4 += pys.Current(u_2.bar(), 'mu', u_a('-')).dot(Gluon_b) * pys.Current(u_a.bar('-'), 'mu', v_1).dot(Gluon_a)
+term_4  = Current(u_2.bar(), 'mu', u_a('+')).dot(Gluon_b) * Current(u_a.bar('+'), 'mu', v_1).dot(Gluon_a)
+term_4 += Current(u_2.bar(), 'mu', u_a('-')).dot(Gluon_b) * Current(u_a.bar('-'), 'mu', v_1).dot(Gluon_a)
 
-A_t = - i * term_4 / pys.s(pa, p1)
+A_t = - i * term_4 / s(pa, p1)
 
 print "   -> A_t = ", A_t[pols]
 
@@ -57,12 +57,12 @@ print "   -> A_t = ", A_t[pols]
 # u-channel
 #
 
-term_5  = pys.Current(u_2.bar(), 'mu', u_a('+')).dot(Gluon_a) * pys.Current(u_a.bar('+'), 'mu', v_1).dot(Gluon_b)
-term_5 += pys.Current(u_2.bar(), 'mu', u_a('-')).dot(Gluon_a) * pys.Current(u_a.bar('-'), 'mu', v_1).dot(Gluon_b)
-term_6  = pys.Current(u_2.bar(), 'mu', u_2('+')).dot(Gluon_a) * pys.Current(u_2.bar('+'), 'mu', v_1).dot(Gluon_b)
-term_6 += pys.Current(u_2.bar(), 'mu', u_2('-')).dot(Gluon_a) * pys.Current(u_2.bar('-'), 'mu', v_1).dot(Gluon_b)
+term_5  = Current(u_2.bar(), 'mu', u_a('+')).dot(Gluon_a) * Current(u_a.bar('+'), 'mu', v_1).dot(Gluon_b)
+term_5 += Current(u_2.bar(), 'mu', u_a('-')).dot(Gluon_a) * Current(u_a.bar('-'), 'mu', v_1).dot(Gluon_b)
+term_6  = Current(u_2.bar(), 'mu', u_2('+')).dot(Gluon_a) * Current(u_2.bar('+'), 'mu', v_1).dot(Gluon_b)
+term_6 += Current(u_2.bar(), 'mu', u_2('-')).dot(Gluon_a) * Current(u_2.bar('-'), 'mu', v_1).dot(Gluon_b)
 
-A_u = i * (term_5 - term_6) / pys.s(pa, p2)
+A_u = i * (term_5 - term_6) / s(pa, p2)
 
 print "   -> A_u = ", A_u[pols]
 
@@ -87,7 +87,7 @@ for i, pol in enumerate(pols):
 
 	print "\n   -> Matrix Element squared for polarisation", pol, " = ",
 
-	temp = pys.g_s ** 4 * (16.0 * pys.abs2(j0[i]) + 16.0 * pys.abs2(j1[i]) - 4.0 * (j0[i] * j1[i].conjugate()).real) / 3.0
+	temp = g_s ** 4 * (16.0 * abs2(j0[i]) + 16.0 * abs2(j1[i]) - 4.0 * (j0[i] * j1[i].conjugate()).real) / 3.0
 
 	# Priunt indiviual amplitudes
 	print temp
